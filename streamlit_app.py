1120c830dccbdc9f9866250c0093e5c3e1795e0c
KrishnaFIXITY <109794889+KrishnaFIXITY@users.noreply.github.com>
2022-07-31 07:35:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title('my parents new healthy Diner')
+streamlit.header('break fast menu')
+streamlit.text('egg & fruits')
+streamlit.text('spanish  egg role, milk buttar')
+
+

1ea8b36d1ed5fc2b19703c7697bb2957aaab6045
KrishnaFIXITY <109794889+KrishnaFIXITY@users.noreply.github.com>
2022-08-08 03:43:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,11 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-streamlit.header("Fruityvice Fruit Advice!")
+

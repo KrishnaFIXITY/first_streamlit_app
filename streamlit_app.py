39170e6fdb0387d304350a0d6b7ceed001fb3879
KrishnaFIXITY <109794889+KrishnaFIXITY@users.noreply.github.com>
2022-08-08 07:24:26 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,12 @@ select demo_db.public.grader(step, (actual = expected), actual, expected, descri
 
 import snowflake.connector
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+
 

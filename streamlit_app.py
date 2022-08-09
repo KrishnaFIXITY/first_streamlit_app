a7fd0557400c213866864161c649610d8ca03cc5
KrishnaFIXITY <109794889+KrishnaFIXITY@users.noreply.github.com>
2022-08-08 21:41:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
 
 #  import the valuesjson file python libery! 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())

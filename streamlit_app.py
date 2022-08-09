9bac933b485d8dae9db03ea3a06f3428e3afcf95
KrishnaFIXITY <109794889+KrishnaFIXITY@users.noreply.github.com>
2022-08-09 07:33:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,21 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 
-streamlit.stop()
+import streamlit as st
+
+st.session_state['answer'] = ''!
+
+st.write(st.session_state)
+
+realans = ['', 'abc', 'edf']
+
+if  st.session_state['answer'] in realans:
+    answerStat = "correct"
+elif st.session_state['answer'] not in realans:
+    answerStat = "incorrect"
+
+st.write(st.session_state)
+st.write(answerStat)
 
 
 

b8d59dcba0323465e6786859aa0380417bd21c6c
KrishnaFIXITY <109794889+KrishnaFIXITY@users.noreply.github.com>
2022-08-09 06:24:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # number fruits present in strem
 streamlit.dataframe(fruityvice_normalized)
 
-streamlit.header('fruityvice fruit advice!')
+
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
